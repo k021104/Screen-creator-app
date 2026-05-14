d0b2a4134ba4cf10c9a06ab43806fec1d925b149
k021104 <krishnachavda1104@gmail.com>
2026-05-14 14:27:53 +0530

    change made for versel deployment error

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import ScreenSetup from './Pages/ScreenSetUp'
 import ScreenBuilder from './Pages/ScreenBuilder'
-import GenerateScreen from './Pages/GenerateScreen';
+import GenerateScreen from './Pages/GenerateScreen'
 
-function App() {
+function App () {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ScreenSetup />} />
-        <Route path="/screenbuilder" element={<ScreenBuilder />} />
-        <Route path="/screengenerate" element={<GenerateScreen />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<ScreenSetup />} />
+          <Route path='/screenbuilder' element={<ScreenBuilder />} />
+          <Route path='/screengenerate' element={<GenerateScreen />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
